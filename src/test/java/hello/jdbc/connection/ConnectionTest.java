34409cea119ea240467e9cb2e883f46ec5bbb0fb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-08 00:54:34 +0900

    test: 기존에 개발했던 DriverManager를 통해 커넥션을 획득하는 방법 테스트

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,23 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+}
