23912bcf06dd1ac80ffb0b3678cbfef494449806
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-08 01:20:39 +0900

    test: 데이터소스 의존관계 주입을 통해 MemberRepositoryV1 테스트

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,57 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 memberRepositoryV1;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        // 기본 DriverManager - 항상 새로운 커넥션 획득
+        // DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //커넥션 풀링: HikariProxyConnection -> JdbcConnection
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        memberRepositoryV1 = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        log.info("start");
+
+        // SAVE
+        Member member = new Member("memberV1", 10000);
+        memberRepositoryV1.save(member);
+
+        // FIND BY ID
+        Member memberById = memberRepositoryV1.findById(member.getMemberId());
+        assertThat(memberById).isNotNull();
+
+        // UPDATE: money: 10000 -> 20000
+        memberRepositoryV1.update(member.getMemberId(), 20000);
+        Member updatedMember = memberRepositoryV1.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // DELETE
+        memberRepositoryV1.delete(member.getMemberId());
+        assertThatThrownBy(() -> memberRepositoryV1.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
