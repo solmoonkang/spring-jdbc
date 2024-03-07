18e5e41562150944515a7d2bc8932d3cc74d4c7d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-08 00:13:41 +0900

    feat: JDBC를 통해 이전에 저장한 데이터를 조회하는 기능 구현

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,11 +5,12 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
 
-    public Member save(Member member) throws SQLException {
+    public void save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values(?, ?)";
 
         Connection connection = null;
@@ -21,12 +22,40 @@ public class MemberRepositoryV0 {
             preparedStatement.setString(1, member.getMemberId());
             preparedStatement.setInt(2, member.getMoney());
             preparedStatement.executeUpdate();
-            return member;
         } catch (SQLException e) {
             log.error("db error", e);
             throw e;
         } finally {
             close(connection, preparedStatement, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, memberId);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Member member = new Member();
+                member.setMemberId(resultSet.getString("member_id"));
+                member.setMoney(resultSet.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(connection, preparedStatement, resultSet);
         }
     }
 
