df79e7345e7878b719f002816b2f74cbf6d4ccbd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-08 00:13:52 +0900

    test: JDBC를 통해 이전에 저장한 데이터를 조회하는 기능 테스트

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,10 +1,14 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 memberRepositoryV0 = new MemberRepositoryV0();
@@ -14,5 +18,10 @@ class MemberRepositoryV0Test {
         // SAVE
         Member member = new Member("memberV0", 10000);
         memberRepositoryV0.save(member);
+
+        // FIND BY ID
+        Member findMember = memberRepositoryV0.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
