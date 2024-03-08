1f628046d12bbabd28ee220f2da0f7a4ea5354fb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-09 03:33:54 +0900

    test: 런타임 예외 사용 변환 테스트

--- src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -18,6 +18,17 @@ class CheckedAppTest {
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            log.info("ex", e);
+        }
+    }
+
     static class Controller {
         Service service = new Service();
 
