a594bb25c29ec82951e7c568e10c4e3405693c3e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-09 18:08:15 +0900

    feat: 스프링 예외 추상화 적용

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
@@ -0,0 +1,137 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * SQLExceptionTranslator 추가
+ */
+@Slf4j
+public class MemberRepositoryV4_2 implements MemberRepository {
+
+    private final DataSource dataSource;
+    private final SQLExceptionTranslator sqlExceptionTranslator;
+
+    public MemberRepositoryV4_2(DataSource dataSource, SQLExceptionTranslator sqlExceptionTranslator) {
+        this.dataSource = dataSource;
+        this.sqlExceptionTranslator = sqlExceptionTranslator;
+    }
+
+    // SAVE() ...
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(member_id, money) values(?, ?)";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, member.getMemberId());
+            preparedStatement.setInt(2, member.getMoney());
+            preparedStatement.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw Objects.requireNonNull(sqlExceptionTranslator.translate("save", sql, e));
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    // FINDBYID() ...
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, memberId);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Member member = new Member();
+                member.setMemberId(resultSet.getString("member_id"));
+                member.setMoney(resultSet.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            throw Objects.requireNonNull(sqlExceptionTranslator.translate("findById", sql, e));
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+    // UPDATE() ...
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, money);
+            preparedStatement.setString(2, memberId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw Objects.requireNonNull(sqlExceptionTranslator.translate("update", sql, e));
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    // DELETE() ...
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id=?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, memberId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw Objects.requireNonNull(sqlExceptionTranslator.translate("delete", sql, e));
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    private void close(Connection connection, Statement statement, ResultSet resultSet) {
+        JdbcUtils.closeResultSet(resultSet);
+        JdbcUtils.closeStatement(statement);
+
+        // ⚠️ 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        DataSourceUtils.releaseConnection(connection, dataSource);
+    }
+
+    private Connection getConnection() throws SQLException {
+        // ⚠️ 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class={}", connection, connection.getClass());
+
+        return connection;
+    }
+}
