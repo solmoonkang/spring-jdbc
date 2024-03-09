570ddd184fcd6eb7c66be98533e89e040bfd9360
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-09 17:10:25 +0900

    feat: MyDbException 런타임 예외 구현

--- src/main/java/hello/jdbc/repository/ex/MyDbException.java
+++ src/main/java/hello/jdbc/repository/ex/MyDbException.java
@@ -4,12 +4,15 @@ public class MyDbException extends RuntimeException {
 
     public MyDbException() {
     }
+
     public MyDbException(String message) {
         super(message);
     }
+
     public MyDbException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public MyDbException(Throwable cause) {
         super(cause);
     }
